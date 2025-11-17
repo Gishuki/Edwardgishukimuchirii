9222f4985f094335b21e3545e3f3405a6596f196
Gishuki <edwardgishuki254@gmail.com>
2025-11-17 20:52:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to handle the display of contact info when the "Contact Me" button is clicked
+function contactMe() {
+  const contactInfo = document.getElementById('contact-info');
+  
+  // Toggle the "show" class to either show or hide the contact info
+  contactInfo.classList.toggle('show');
+}
